c7c856983153be0318e0c29e93ecd5b5eb3ec636
ana 3 <156862849+anastaciaaguirre@users.noreply.github.com>
2024-01-25 15:57:30 -0500

    Add files via upload

---
+++ anastacia aguirre CPCS 207.py
@@ -0,0 +1,58 @@
+Python 3.12.1 (v3.12.1:2305ca5144, Dec  7 2023, 17:23:39) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #the authors' names are: Abbie and Anastacia
+>>> print("hello, world")
+hello, world
+>>> print("Welcome to CPCS 207")
+Welcome to CPCS 207
+>>> print("Hello\nWorld")
+Hello
+World
+>>> print("How\nare\nyou?")
+How
+are
+you?
+>>> print("Today", "is", "Wednesday")
+Today is Wednesday
+>>> print("Today"+"is"+"Wednesday")
+TodayisWednesday
+>>> #print statement display the test inside the quotes
+>>> #\n creates a line break
+>>> #string texts 'added' with the + puts the strings together with no space
+>>> print("123\n45\n6")
+123
+45
+6
+>>> 1+2*3
+7
+>>> (1+2)*3
+9
+>>> 7/3
+2.3333333333333335
+>>> 7//3
+2
+>>> 7%3
+1
+>>> 22/4
+5.5
+>>> 22//4
+5
+>>> 22%4
+2
+>>> # the % essentilly gives the remainder of a division equation
+>>> 3**2
+9
+>>> 2**3
+8
+>>> # yes, python follows the usual order of operations
+>>> # / gives the full quotient while // only gives the whole number
+>>> # % gives the remainder of a division equation
+>>> # ** is the same as an exponent
+>>> (4+3)//2
+3
+>>> 1+2*3**4
+163
+>>> print("2+2=", 2+2)
+2+2= 4
+>>> print("50 pounds is", 50*0.453592, "Kilograms")
+50 pounds is 22.6796 Kilograms
